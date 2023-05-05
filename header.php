d432421a6521a85d6cb2d04ea38d6097cad44f58
letspl <pavliushin.nikita@mail.ru>
2023-05-06 01:14:24 +0300

    main page

--- header.php
+++ header.php
@@ -6,6 +6,29 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CPUniverse</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+<header>
+    <ul>
+        <li>
+            <h1>CPUniverse</h1>
+        </li>
+    </ul>
+    <ul>
+        <li>
+            <a href="">главная</a>
+        </li>
+        <li>
+            <a href="">каталог</a>
+        </li>
+        <li>
+            <a href="">ЗАРЕГИСТРИРОВАТЬСЯ</a>
+        </li>
+        <li>
+            <a href="">ВОЙТИ</a>
+        </li>
+    </ul>
+</header>
 
--- main.php
+++ main.php
@@ -1,3 +1,74 @@
 <?php require_once "header.php"; ?>
 
+<main>
+    <section class="main_page1">
+        <article>
+            <h1>Встрой в свой ПК лучшее</h1>
+            <h2>Или подбери что-нибудь получше</h2>
+            <hr>
+
+            <article>
+                <h1>Кто мы?</h1>
+                <p>
+                    Может быть вы наблюдаете за тем, как рождается легенда, так как мы предоставляем лучшие комплектующие и делаем это быстро
+                </p>
+                <h3>
+                    Наше правило: Лучшее не всегда должно быть с оверпрайсом
+                </h3>
+                <h2>Открой для себя  CPUINIVERSE</h2>
+                <img src="img/IMAGE1.png" alt="">
+            </article>
+        </article>
+        <article>
+            <img src="img/IMAGE.png" alt="">
+        </article>
+    </section>
+
+    <section class="main_page2">
+        <div>
+            <h2>Почему мы?</h2>
+            <h1>Большой выбор</h1>
+            <p>Мы собираем со всего мира самые лучшие комплектующие для того, <br> чтобы каждый мог найти для своего ПК, что новое</p>
+            <a href="catalog.php">Посмотреть каталог</a>
+        </div>
+        <img src="img/4.jpg" alt="">
+    </section>
+
+    <section class="main_page3">
+        <img src="img/4.jpg" alt="">
+        <div>
+            <p>Не забываем, что лучшее <br> не всегда значит дорого</p>
+        </div>
+    </section>
+
+    <section class="main_page4">
+        <img src="img/4.jpg" alt="">
+        <div>
+            <h1>Быстрая доставка</h1>
+            <span>Мы понимаем, как важно время и насколько не хочется порой что-то ждать, поэтому мы специльно каждый день гоняем лысых (наших курьеров), чтобы вы могли получить желаемое быстрее </span>
+        </div>
+    </section>
+
+    <section class="main_page5">
+        <img src="img/4.jpg" alt="">
+        <div>
+            <h1>Даем скидки всем</h1>
+            <span>Хорошие комплектующие не каждому могут быть по карману, поэтому мы специально делаем нашем покупателям скидки, так и для всех, так и персональные</span>
+        </div>
+    </section>
+
+    <div class="back"></div>
+
+    <section class="main_page6">
+        <h2>Хотите что-то спросить лично?</h2>
+        <h3>На такой случай мы вам обязательно ответим, как можно скорее и предоставим качественную помощь</h3>
+        <label for="">
+            <input placeholder="email" type="text">
+            <input placeholder="имя" type="text">
+            <input placeholder="сообщение" type="text">
+            <input value="Отправить" type="button">
+        </label>
+    </section>
+</main>
+
 <?php require_once "footer.php"; ?>
